dfee61b9522e05fc15a34411364afe5ecb0e48ff
Vipul Rai <tp_vrai@nutreco.com>
2023-01-23 16:13:04 +0100

    counterfit demo for camera

---
+++ counterfit-demo/app.py
@@ -0,0 +1,19 @@
+from counterfit_connection import CounterFitConnection
+
+CounterFitConnection.init("127.0.0.1", 5000)
+
+import io
+
+from counterfit_shims_picamera import PiCamera
+# for rs py -  from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.rotation = 0
+
+image = io.BytesIO()
+camera.capture(image, "jpeg")
+image.seek(0)
+
+with open("image.jpg", "wb") as image_file:
+    image_file.write(image.read())
